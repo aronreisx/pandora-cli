da9ef0d21cf22dd40bd874fad40bc58b93048019
Aron Reis <aronreis2@gmail.com>
2023-02-26 18:30:48 +0000

    style: conform code to the print width limit

--- pkg/az/az.go
+++ pkg/az/az.go
@@ -8,14 +8,30 @@ import (
 
 type CreatePullRequestArgs struct {
 	Title string
-	ID string
+	ID    string
 }
 
 func CreatePullRequest(title string, id string, sourceBranch string) string {
 	resource := "senses"
 	targetBranch := "master"
 
-	cmd := exec.Command("az", "repos", "pr", "create", "--title", title, "--delete-source-branch", "--auto-complete", "--work-items", id, "-r", resource, "-s", sourceBranch, "-t", targetBranch)
+	cmd := exec.Command("az",
+		"repos",
+		"pr",
+		"create",
+		"--title",
+		title,
+		"--delete-source-branch",
+		"--auto-complete",
+		"--work-items",
+		id,
+		"-r",
+		resource,
+		"-s",
+		sourceBranch,
+		"-t",
+		targetBranch)
+
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		app.Error("")
