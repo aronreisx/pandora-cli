994f545082ff7af22a9c001cf5c9428d52374103
Aron Reis <aronreis2@gmail.com>
2023-02-26 17:44:50 +0000

    feat: add custom azure cli package

---
+++ pkg/az/az.go
@@ -0,0 +1,24 @@
+package az
+
+import (
+	"os/exec"
+
+	"github.com/aronreisx/go-intro/pkg/app"
+)
+
+type CreatePullRequestArgs struct {
+	Title string
+	ID string
+}
+
+func CreatePullRequest(title string, id string, sourceBranch string) string {
+	resource := "senses"
+	targetBranch := "master"
+
+	cmd := exec.Command("az", "repos", "pr", "create", "--title", title, "--delete-source-branch", "--auto-complete", "--work-items", id, "-r", resource, "-s", sourceBranch, "-t", targetBranch)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		app.Error("")
+	}
+	return string(output)
+}
